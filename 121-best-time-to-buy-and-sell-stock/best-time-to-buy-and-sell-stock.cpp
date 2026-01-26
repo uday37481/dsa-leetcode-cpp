0bbac73bf76e824c62bca5ff141fb31c62690f12
Uday Nagar <uvnagar370823@kkwagh.edu.in>
2026-01-26 21:28:03 +0530

    Time: 0 ms (100.00%) | Memory: 97.3 MB (59.82%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,24 @@
+class Solution 
+{
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int buy_price = prices[0] ;
+        int profit = 0;
+
+        for(int i=1 ; i< prices.size(); i++)
+        {
+          if (prices[i] < buy_price)
+          {
+            buy_price = prices[i] ;
+          }
+          else
+          {
+            int current_profit = prices[i] - buy_price ;
+            profit = max(current_profit, profit) ;
+          }
+        }
+
+        return profit;        
+    }
+};
