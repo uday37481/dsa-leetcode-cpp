e7b22ca9d02e2c1fd862d879d03ccace252577e9
Uday Nagar <uvnagar370823@kkwagh.edu.in>
2026-07-16 23:05:00 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (35.01%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int search(vector<int>& A, int tar) 
+    {
+        int st = 0, end = A.size() - 1;
+
+        while (st <= end) 
+        {
+            int mid = st + (end - st) / 2;
+
+            if (A[mid] == tar) 
+            {
+                return mid;
+            }
+
+            if (A[st] <= A[mid]) 
+            {   // left sorted    
+            if (A[st] <= tar && tar <= A[mid]) 
+            {
+                end = mid - 1;
+            } 
+            else 
+            {
+                st = mid + 1;
+            }
+            } 
+            else 
+            {                // right sorted
+                if (A[mid] <= tar && tar <= A[end]) 
+                {
+                    st = mid + 1;
+                } 
+                else 
+                {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
